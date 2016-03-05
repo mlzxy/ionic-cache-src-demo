7545a8189387727709dd713f6e9cc04e4935f755
Xinyu <xyzhang.me@aliyun.com>
2016-03-05 01:57:35 -0800

    update ionic-cache-src

--- www/lib/ionic-cache-src/ionic-cache-src.js
+++ www/lib/ionic-cache-src/ionic-cache-src.js
@@ -235,6 +235,8 @@
                                     return $window.cordova.file.documentsDirectory;
                                 case 'Android':
                                     return $window.cordova.file.dataDirectory;
+                                case 'windows':		
+                                    return $window.cordova.file.dataDirectory;
                             }
                             return '';
                         };
@@ -285,8 +287,7 @@
 
                         
                         $ionicPlatform
-                            .ready()
-                            .then(function() {
+                            .ready(function() {
                                 scope.$watch('attrs.cacheSrc',
                                                function() {
                                                    // debugger;
